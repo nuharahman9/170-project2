3be58506de29437d67c21a6ca5a05ff421d50d92
nuharahman9 <nrahm009@ucr.edu>
2023-06-10 17:42:09 -0700

    nearest neighbor

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from Node import Node 
+from Classifier import Classifier
+import numpy 
 
-n = 4
-node = Node(n, [])
+# n = 4
+# node = Node(n, [])
 
-(node.forwardSelection())
+# (node.forwardSelection())
+
+tempClass = Classifier()
+p1 = tempClass.df.iloc[0].to_numpy()
+p2 = tempClass.df.iloc[1].to_numpy()
+# print(type(p1))
+# print(tempClass.euclideanDistance(p1, p2))
+
+print(tempClass.test(3))
+
+
+# print(tempClass.df)
