285aca58218bb6c8dcef32649278e7ea9a7d12b5
nuharahman9 <nrahm009@ucr.edu>
2023-06-10 23:08:30 -0700

    updated driver

--- main.py
+++ main.py
@@ -4,38 +4,26 @@ from Validator import Validator
 import numpy 
 import pandas as pd
 
-# n = 4
-# node = Node(n, [])
 
-# (node.forwardSelection())
-
-# tempClass = Classifier("/Users/terencemui/Documents/CS170/170-project2/small-test-dataset.txt")
-# p1 = tempClass.df.iloc[0].to_numpy()
-# p2 = tempClass.df.iloc[1].to_numpy()
-# print(type(p1))
-# print(tempClass.euclideanDistance(p1, p2))
-
-# print("test")
-
-# print(tempClass.test(3))
-
-
-# print(tempClass.df)
-
-
-# filepath = "/Users/terencemui/Documents/CS170/170-project2/large-test-dataset-1.txt"
-# testValid = Validator()
-# testValid.validate([1, 15, 27], filepath)
-
-# filepath = "/Users/terencemui/Documents/CS170/170-project2/small-test-dataset.txt"
-# testValid = Validator()
-# print(testValid.validate([3, 5], filepath))
-
-# filepath = "/Users/terencemui/Documents/CS170/170-project2/small-test-dataset.txt"
-# filepath = "/Users/terencemui/Documents/CS170/170-project2/large-test-dataset-1.txt"
-# filepath = "/Users/terencemui/Documents/CS170/170-project2/CS170_Spring_2023_Small_data__95.txt"
-filepath = "/Users/terencemui/Documents/CS170/170-project2/CS170_Spring_2023_Large_data__95.txt"
-
+filepath = None
+option = input("welcome to our 170 project 2!\nTo select a filepath, please enter a number:\n1. Sample small dataset from part 1\n2. Sample large dataset from part 1\n3. Our own small dataset\n4. Our own large dataset\nType any other integer to quit :+)\nPlease enter a number:\n")
+if option == 1: 
+    filepath = "/Users/terencemui/Documents/CS170/170-project2/small-test-dataset.txt"
+elif option == 2: 
+    filepath = "/Users/terencemui/Documents/CS170/170-project2/large-test-dataset-1.txt"
+elif option == 3: 
+    filepath = "/Users/terencemui/Documents/CS170/170-project2/CS170_Spring_2023_Small_data__95.txt"
+elif option == 4: 
+    filepath = "/Users/terencemui/Documents/CS170/170-project2/CS170_Spring_2023_Large_data__95.txt"
+else: 
+    quit  
 node = Node([], filepath)
-node.forwardSelection()
+
+option = input("please select the desired option for search:\n1. Forward selection\n2. Backward elimination\nType anything else to quit :+)\nPlease enter a number:\n")
+if option == 1: 
+    node.forwardSelection()
+if option == 2: 
+    node.backwardElimination()
+else: 
+    quit
 # node.backwardElimination()
